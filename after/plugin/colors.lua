cb014de604ecde5f5f771b4e7a93c1e1f447560f
Joel Rocha <joelxr@gmail.com>
2024-08-21 16:57:37 -0300

    .

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -22,3 +22,9 @@ if kittyTheme == "Rosé Pine Moon" then
 else
   light()
 end
+
+vim.cmd("highlight Cursor guibg=#B90E0A ctermbg=67")
+vim.cmd("highlight iCursor guibg=#B90E0A ctermbg=229")
+vim.cmd("highlight rCursor guibg=#B90E0A ctermbg=124")
+
+vim.cmd("set guicursor=n-v-c:block-Cursor/lCursor,i-ci-ve:ver100-iCursor,r-cr:block-rCursor,o:hor50-Cursor/lCursor,sm:block-iCursor,a:blinkwait1000-blinkon500-blinkoff250")
